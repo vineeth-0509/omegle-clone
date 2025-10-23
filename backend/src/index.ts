c25425e178a3672edab4b64e69164f2ebc7deb1e
Vineeth-0509 <vineeththungani@gmail.com>
2025-10-23 20:55:03 +0530

    changed the error signals in the index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,24 +1,23 @@
-import {Socket} from "socket.io"
+import { Socket } from "socket.io";
 import http from "http";
-const express = require("express");
-import {Server} from "socket.io"
+import express, { Request, Response } from "express";
+import { Server } from "socket.io";
 import { UserManager } from "./managers/UserManager";
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-io.on('connection', (socket:Socket)=>{
-    console.log('a user connected');
-    const addUsers = new UserManager();
-    // addUsers.addUser(req.params, socket.id)
-    app.get("/createRoom", async(req:Request, res:Response)=>{
-        const params = await  req.json();
-        addUsers.addUser(params, socket)        
-})
-})
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected");
+  const addUsers = new UserManager();
+  // addUsers.addUser(req.params, socket.id)
+  app.get("/createRoom", async (req: Request, res: Response) => {
+    const params = await req.body();
+    const data = addUsers.addUser(params, socket);
+  });
+});
 
-
-server.listen(3000,()=>{
-    console.log('Listenig on 3000')
-})
+server.listen(3000, () => {
+  console.log("Listenig on 3000");
+});
